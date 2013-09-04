78410df0d4934be167490a2e803347b836ee39e8
Tom Close <tom.close@cantab.net>
2013-09-04 20:44:58 +0100

    Gruntfile now working.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,30 +7,63 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     copy: {
       main: {
-        expand: true,
-        cwd: 'app/',
-        src: '*',
-        dest: 'dest/',
-        filter: 'isFile'
+        files: [
+        {
+          expand: true,
+          cwd: 'app/',
+          src: '*',
+          dest: 'dist/',
+          filter: 'isFile'
+        },
+        {
+          expand:true, cwd: 'app/images', src: '*', dest: 'dist/images'
+        },
+        {
+          src: 'app/bower_components/jquery/jquery.js',
+          dest: 'dist/js/jquery.js',
+        },
+        {
+          src: 'app/bower_components/bootstrap/dist/js/bootstrap.js',
+          dest: 'dist/js/bootstrap.js',
+        }
+        ]
       },
     },
-    clean: ['dest/'],
+    clean: ['dist/'],
     less: {
       files: {
         expand: true,
         cwd: 'app/styles/',
         src: '*.css',
-        dest: 'dest/css/'
+        dest: 'dist/styles/'
       }
-    }
+    },
+    watch: {
+      less: {
+        // We watch and compile sass files as normal but don't live reload here
+        files: ['app/styles/*.css'],
+        tasks: ['less'],
+      },
+      copy: {
+        files: ['app/*'],
+        tasks: ['copy']
+      },
+      livereload: {
+        // Here we watch the files the sass task will compile to
+        // These files are sent to the live reload server after sass compiles to them
+        options: { livereload: true },
+        files: ['dist/**/*'],
+      },
+    },
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
-  grunt.registerTask('default', ['clean', 'copy', 'less']);
+  grunt.registerTask('default', ['clean', 'copy', 'less', 'watch']);
 
 };
